ea16d2a3c2f2fe2ed20d38365300f4fd2031b34e
Myresa <aberneli@z4r8p1.42lyon.fr>
2024-12-02 14:21:13 +0100

    rebased to the new build architecture

---
+++ inc/SocketW.hpp
@@ -0,0 +1,63 @@
+#ifndef SOCKETW_HPP
+# define SOCKETW_HPP
+
+# include <sstream>
+# include <string>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <exception>
+
+# define MSG_SEPARATOR "\n"
+# define MSG_MAXLEN 1024
+
+class SocketW
+{
+	private:
+	std::ostringstream	os;
+	std::ostringstream	is;
+	int					fd;
+	bool				consumable;
+
+		public:
+	SocketW(int newFd);
+	~SocketW();
+
+	/* Send the buffer built with operator<< and appends MSG_SEPARATOR at the end */
+	int			Send();
+
+	/* Read data from the socket buffer, returns true if a message is available for "consume" */
+	bool		Receive();
+	
+	/* Get the latest message read from the socket if consumable */
+	std::string	Consume();
+
+	/* Tells if we have a message available */
+	bool        CanConsume() const;
+
+	/* Check if os::operator<< went well */
+	explicit	operator bool() const;
+
+	/* Exception throw by Receive() if the remote socket was closed */
+	class EConnectionClosed : public std::exception
+	{
+			public:
+		virtual const char *what() const throw();
+	};
+
+	/* Exception thrown by Receive() when recv returns < 0 */
+	class ERecvError : public std::exception
+	{
+			public:
+		virtual const char *what() const throw();
+	};
+	
+	/* Appends data to the output buffer */
+	template <typename T>
+	SocketW& operator<<(const T &val)
+	{
+		os << val;
+		return (*this);
+	}
+};
+
+#endif
---
+++ src/SocketW.cpp
@@ -0,0 +1,80 @@
+#include "SocketW.hpp"
+#include <sys/errno.h>
+#include <cstring>
+#include <cmath>
+
+SocketW::SocketW(int newFd) : os(), is(), fd(newFd), consumable(false)
+{
+	
+}
+
+SocketW::~SocketW()
+{
+
+}
+
+int	SocketW::Send()
+{
+	std::string tmp;
+
+	tmp = os.str();
+	os.clear();
+	os.str(std::string());
+	tmp = tmp.substr(0, MSG_MAXLEN) + MSG_SEPARATOR;
+	return (send(fd, tmp.c_str(), tmp.size(), 0));
+}
+
+bool	SocketW::Receive()
+{
+	char	buffer[4096 + 1];
+	int		rd;
+	
+	rd = recv(fd, (void *)buffer, 4096, 0);
+	
+	if (rd < 0)
+		throw ERecvError();
+	if (rd == 0)
+		throw EConnectionClosed();
+
+	buffer[rd] = '\0';
+	is << buffer;
+	consumable = (is.str().find(MSG_SEPARATOR) != std::string::npos);
+	return (consumable);
+}
+
+std::string	SocketW::Consume()
+{
+	std::string tmp;
+	size_t		pos;
+
+	if (!consumable)
+		return (std::string("")); /* bad consume, throw exception maybe ? */
+
+	tmp = is.str();
+	pos = tmp.find(MSG_SEPARATOR);
+	is.str(std::string(tmp.substr(pos + 1)));
+	is.clear();
+	tmp = tmp.substr(0, std::min(pos, (size_t)MSG_MAXLEN));
+	consumable = (is.str().find(MSG_SEPARATOR) != std::string::npos); /* Set the Consummable flag */
+	return (tmp);
+}
+
+bool	SocketW::CanConsume() const
+{
+	return (consumable);
+}
+
+SocketW::operator bool() const
+{
+    return (os.operator bool());
+}
+
+const char *SocketW::EConnectionClosed::what() const throw()
+{
+	return ("Remote connection dropped");
+}
+
+const char *SocketW::ERecvError::what() const throw()
+{
+	return (std::strerror(errno));
+}
