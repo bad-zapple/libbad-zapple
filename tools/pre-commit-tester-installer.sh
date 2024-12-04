6a7f95c03a375ea859732aa978309857cac9eb1d
Brieuc Poumeau De Lafforest <delafforest.brieuc@gmail.com>
2024-12-04 15:47:00 +0100

    installer to make test at commit

--- tools/pre-commit-tester-installer.sh
+++ tools/pre-commit-tester-installer.sh
@@ -1,3 +1,16 @@
-echo "make lsp && make run-test" >> `cat .git | awk 'NR==1 {print $2; exit}'`/hooks/pre-commit
+echo "$PRE_COMMIT_FILE"
 
-# chmod 777 ./.git/hooks/pre-commit
+
+GIT="./.git"
+
+if [ -e "$GIT" ]; then
+    if [ -f "$GIT" ]; then
+	PRE_COMMIT_FILE=`cat $GIT | awk 'NR==1 {print $2; exit}'`/hooks/pre-commit
+    elif [ -d "$GIT" ]; then
+	PRE_COMMIT_FILE=./.git/hooks/pre-commit
+    fi
+else
+    echo "not in a repository"
+fi
+echo "make lsp && make run-test" >> $PRE_COMMIT_FILE
+chmod +x $PRE_COMMIT_FILE
