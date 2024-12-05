bb61bb70fe212198fe05a92d84ebfc2e5ce08287
Brieuc Poumeau De Lafforest <delafforest.brieuc@gmail.com>
2024-12-05 13:48:28 +0100

    Build: add flags to disable compilation on warning and fix issues of compilation

--- tests/basic.cpp
+++ tests/basic.cpp
@@ -7,10 +7,6 @@ int main(int ac, char *av[] ) {
 	return result;
 }
 
-int tests_basic(int ac, char **av) {
-	return main(ac, av);
-}
-
 
 TEST_CASE( "Dummy is tested", "[dummy]") {
 	REQUIRE( dummy() == 0);
