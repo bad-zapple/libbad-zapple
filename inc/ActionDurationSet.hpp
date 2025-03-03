9641da7fd58051c2372a4f4542008d6afb55c32f
Brieuc Poumeau De Lafforest <delafforest.brieuc@gmail.com>
2025-03-03 16:20:51 +0100

    feat: creation of empty class ActionDurationSet in GameRuleSet

---
+++ inc/ActionDurationSet.hpp
@@ -0,0 +1,49 @@
+#ifndef ACTIONDURATIONSET_HPP
+# define ACTIONDURATIONSET_HPP
+
+# include <cstddef>
+# include <string>
+
+class ActionDurationSet {
+		private:
+	size_t tt_advance;
+	size_t tt_turn;
+
+	size_t tt_see;
+
+	size_t tt_kick;
+	size_t tt_incantate;
+
+	size_t tt_take;
+	size_t tt_put_down;
+
+	size_t tt_broadcast;
+	size_t tt_get_unused_connections;
+
+	size_t tt_egg_layed;
+	size_t tt_egg_hatched;
+	size_t tt_egg_rotten;
+		
+		public:
+	ActionDurationSet(const std::string &config);
+
+	size_t get_tt_advance() const;
+	size_t get_tt_turn() const;
+
+	size_t get_tt_see() const;
+
+	size_t get_tt_kick() const;
+	size_t get_tt_incantate() const;
+
+	size_t get_tt_take() const;
+	size_t get_tt_put_down() const;
+
+	size_t get_tt_broadcast() const;
+	size_t get_tt_get_unused_connections() const;
+
+	size_t get_tt_egg_layed() const;
+	size_t get_tt_egg_hatched() const;
+	size_t get_tt_egg_rotten() const;
+};
+
+#endif
--- inc/GameRuleSet.hpp
+++ inc/GameRuleSet.hpp
@@ -5,28 +5,28 @@
 # include <string>
 # include <vector>
 
-// # include "ActionSpeed.hpp" insert here or
-
+# include "ActionDurationSet.hpp"
 
 class GameRuleSet {
-	public:
-		GameRuleSet(const std::string &serialized_config);
-		~GameRuleSet();
+		public:
 
-		const std::string &get_serialized_config() const ;
+		private:
+	size_t map_w;
+	size_t map_h;
+	size_t tick_speed;
+	std::vector<std::string> stones;
+	std::vector<std::string> teams;
+	size_t max_clients_by_team;
+	size_t number_of_team;
+	size_t max_clients;
+	std::string serialized_config;
+	ActionDurationSet durations;
 
-	private:
-		size_t map_w;
-		size_t map_h;
-		size_t tick_speed;
-		std::vector<std::string> stones;
-		std::vector<std::string> teams;
-		size_t max_clients_by_team;
-		size_t number_of_team;
-		size_t max_clients;
-		std::string serialized_config;
-//		ActionSpeed actions_speed; TODO uncomment when  Action Speed will be created
-//
+		public:
+	GameRuleSet(const std::string &serialized_config);
+	~GameRuleSet();
+
+	const std::string &get_serialized_config() const ;
 };
 
 #endif
