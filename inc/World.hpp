6eb06370cd96dca80608bf9904570b11586e9e4c
Myresa <myresa.bernelin@gmail.com>
2025-03-03 15:43:26 +0100

    Basics of World and Cell class, along with a ViewIterator draft

--- inc/World.hpp
+++ inc/World.hpp
@@ -2,16 +2,43 @@
 # define WORLD_HPP
 
 # include <stdint.h>
+# include <vector>
+# include <string>
 
 class World
 {
+		public:
+	class Cell
+	{
+			private:
+		std::vector<int> inventory;
+	
+			public:
+		Cell();
+		~Cell();
+
+		std::string GetViewContent() const;
+		const std::vector<int>& GetRawContent() const;
+		void PlaceItem(int itemid);
+		int PickItem(int itemid);
+	};
+
 		private:
 	uint32_t width;
 	uint32_t height;
-	 
+
 		public:
 	World();
 	~World();
+
+	const Cell& GetCell(uint32_t x, uint32_t y) const;
+	Cell& GetCell(uint32_t x, uint32_t y);
+	void GetDimensions(uint32_t &w, uint32_t &h) const;
+
+	/*
+		ViewIterator built with Direction+Depth(player level),
+			which will automatically map the view cone to implement "VOIR"
+	*/
 };
 
 #endif
