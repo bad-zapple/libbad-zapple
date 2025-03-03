e7938d6652375a5ceb484b6f9037cd9b0c46e99b
Brieuc Poumeau De Lafforest <delafforest.brieuc@gmail.com>
2025-03-03 14:54:14 +0100

    feat: GameRuleSet method prototype

--- inc/GameRuleSet.hpp
+++ inc/GameRuleSet.hpp
@@ -5,17 +5,28 @@
 # include <string>
 # include <vector>
 
+// # include "ActionSpeed.hpp" insert here or
+
+
 class GameRuleSet {
+	public:
+		GameRuleSet(std::string serialized_config);
+		~GameRuleSet();
+
+		std::string get_serialized_config() const ;
+
 	private:
-		size_t map_h;
-		size_t map_w;
-		size_t tick_speed;
-		std::vector<std::string> stones;
-		std::vector<std::string> teams;
-		size_t max_clients_by_team;
-		size_t number_of_team;
-		size_t max_clients;
-		ActionSpeed actions_speed;
+		const size_t map_h;
+		const size_t map_w;
+		const size_t tick_speed;
+		const std::vector<std::string> stones;
+		const std::vector<std::string> teams;
+		const size_t max_clients_by_team;
+		const size_t number_of_team;
+		const size_t max_clients;
+		const std::string serialized_config;
+//		ActionSpeed actions_speed; TODO uncomment when  Action Speed will be created
+//
 };
 
 #endif
